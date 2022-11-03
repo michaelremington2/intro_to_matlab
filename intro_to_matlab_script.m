9f4bc4aa55a283092119eac40717e9e98534068b
michaelremington2 <michaelremington2@gmail.com>
2022-11-03 15:58:51 -0700

    started workshop script and presentation.

---
+++ intro_to_matlab_script.m
@@ -0,0 +1,13 @@
+clear; close all, clc
+
+% This is a comment. Below are some mathematical opperations.
+%% Double percent signs are a section Break
+
+% Below are some mathematical operations and other objects.
+a=3;
+b=5;
+c=(a*b+b^a)/a;
+word = 'August';
+% Check out the workspace pannel to see a list of our variables.
+
+%% Variables and built in functions
